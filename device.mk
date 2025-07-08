09f424d31d265c5a5b1d9cb6b7dfdfa15aefb79a
Massatrio16 <dimassetosatrio@gmail.com>
2025-07-08 10:08:09 +0700

    add service

--- device.mk
+++ device.mk
@@ -25,8 +25,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Fastbootd
 PRODUCT_PACKAGES += \
-    fastbootd \
-    android.hardware.fastboot@1.0-impl-mtk.so
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
